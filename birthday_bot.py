23c284ac07760525052a3d7d50f61f8ff2c40968
LEv145 <you@example.com>
2024-05-04 12:29:38 +0200

    Обновлён код самим LEv145

--- birthday_bot.py
+++ birthday_bot.py
@@ -1,18 +1,26 @@
-import vk_api
-import telebot
 from datetime import datetime
 import sqlite3
+from threading import Thread
+import logging
+from os import getenv
+
+import vk_api
+import telebot
+
+
+CHAT_ID = -1001895882875
+TELEGRAM_BOT_TOKEN = getenv("TBB__TELEGRAM_BOT_TOKEN")
+VK_BOT_TOKEN = getenv("TBB__VK_BOT_TOKEN")
 
 nums = {"1": "01", "2": "02", "3": "03", "4": "04", "5": "05", "6": "06", "7": "07", "8": "08", "9": "09"}
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
 
 
-def bdate() -> dict:
+def get_bdate() -> dict:
     file = open("PeopleInGroup.txt", 'a')
     bd = {}
-    vk_session = vk_api.VkApi(token="vk1.a.KroTG7TGWSgwQvFQezXp8no4ADi0vaWVgo5yBmwfLO24FKe6v2PIEMsbBeya"
-                                    "-i1bBbQlL8GhlcjFL62l8hGJrUDOeAu1MyXIvSEyAbh5zfn"
-                                    "-57XNQmagg70MKuL9pF_pdt6HGnRK1KVWUW7o9s8qkmkwhGnroKWPLpfaVECEHCAujircy5_"
-                                    "-MUToTUQ3P446kX5b2VHfbaOAtRrJWXZQtg")
+    vk_session = vk_api.VkApi(token=VK_BOT_TOKEN)
     data = vk_session.method('messages.getChatUsers', dict(chat_id=36, fields="bdate", name_case="gen"))
     file.write(str(len(data)) + '\n')
     for i in data:
@@ -30,8 +38,8 @@ def bdate() -> dict:
     return bd
 
 
-def bdate_data():
-    bd = bdate()
+def update_bdate_data():
+    bd = get_bdate()
     conn = sqlite3.connect('bdate.sql')
     cursor = conn.cursor()
     file = open("PeopleInGroup.txt")
@@ -54,33 +62,35 @@ def bdate_data():
     conn.close()
 
 
-bot = telebot.TeleBot("6999131337:AAE5-xPaBdkK8wtJYG5-7WsnXlJl4YEPeuw")
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 
-@bot.message_handler(commands=['start'])
-def birthday_bot(message: telebot.types.Message):
-    i=0
-    bot.send_message(message.chat.id, "Hello!")
+def birthday_bot_event():
+    i = 0
+    log.info("Hello!")
     now_date = datetime.now().strftime("%d.%m")
     date_update = True
-    bdate_data()
-    conn = sqlite3.connect('bdate.sql')
+    update_bdate_data()
+    conn = sqlite3.connect("bdate.sql")
     cursor = conn.cursor()
 
-    cursor.execute('SELECT * FROM bdates')
+    cursor.execute("SELECT * FROM bdates")
     bd = cursor.fetchall()
     while True:
         if date_update:
             for person in bd:
                 if person[2] != "":
-                    if datetime.now().strftime("%d.%m")== person[2]:#
-                        bot.send_message(message.chat.id, f"Сегодня день рождения у {person[1]}!")
-                        i+=1
-            print(i)
+                    if datetime.now().strftime("%d.%m") == person[2]:
+                        bot.send_message(CHAT_ID, f"Сегодня день рождения у {person[1]}!")
+                        i += 1
+            log.info(i)
         if datetime.now().strftime("%d.%m") == now_date:
             date_update = False
         else:
             date_update = True
 
 
-bot.polling(none_stop=True)
+event_thread = Thread(target=birthday_bot_event, daemon=True)
+bot_thread = Thread(target=bot.infinity_polling)
+bot_thread.start()
+event_thread.start()
